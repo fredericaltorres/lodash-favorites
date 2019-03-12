52caf69ca46e6896334286f61d3229df54a69070
Frederic Torres <fredericaltorres@gmail.com>
2019-03-12 18:03:19 -0400

    update

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 /*
-
-    https://lodash.com/ 
-
+    https://lodash.com/
 */
 
 var _ = require('lodash');
 
-
-console.log(`lodash favorites`);
+console.log(`--- lodash favorites ---`);
 
 var users = [
-    { 'user': 'barney',  'active': false },
-    { 'user': 'fred',    'active': false },
-    { 'user': 'pebbles', 'active': true }
-  ];
+  { 'user': 'barney',  'active': false },
+  { 'user': 'fred',    'active': false },
+  { 'user': 'pebbles', 'active': true }
+];
   
-  let result = 0;
-  result = _.findIndex(users, function(o) { return o.user == 'barney'; });
-  console.log(result);
+let result = 0;
+
+// https://lodash.com/docs/4.17.11#findIndex
+result = _.findIndex(users, function(o) { return o.user == 'barney'; });
+console.log(result);
+
+result = _.findIndex(users, { 'user': 'fred', 'active': false });
+console.log(result);
