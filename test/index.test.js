8c003cfc1fdc8825f26a59bdee2ca06700a25c8b
Frederic Torres <fredericaltorres@gmail.com>
2019-03-13 15:12:27 -0400

    Update

--- test/index.test.js
+++ test/index.test.js
@@ -13,45 +13,44 @@ var users = [
   
 describe('Array Suite', () => {
 
-  describe('findIndex', () => {
+    describe('findIndex', () => {
 
-        // https://lodash.com/docs/4.17.11#findIndex
-        it('findIndex based on predicat', () => {
+          // https://lodash.com/docs/4.17.11#findIndex
+          it('findIndex based on predicat', () => {
 
-            const result = _.findIndex(users, function(o) { return o.user == 'barney'; });
-            expect(result).to.equal(0);
-        });
+              const result = _.findIndex(users, function(o) { return o.user == 'barney'; });
+              expect(result).to.equal(0);
+          });
 
-        it('findIndex based on instance', () => {
+          it('findIndex based on instance', () => {
 
-            const result = _.findIndex(users, { 'user': 'fred', 'active': false });      
-            expect(result).to.equal(1);
-        });
-    });      
-    describe('findIndex', () => {      
+              const result = _.findIndex(users, { 'user': 'fred', 'active': false });      
+              expect(result).to.equal(1);
+          });
+      });      
+      describe('findIndex', () => {      
 
-        it('intersection', () => {
+          it('intersection', () => {
 
-            const result = _.intersection([2, 1], [2, 3]);
-            assert.isTrue(Array.isArray(result));
-            assert.deepEqual([2], result);
-        });
-    });
+              const result = _.intersection([2, 1], [2, 3]);
+              assert.isTrue(Array.isArray(result));
+              assert.deepEqual([2], result);
+          });
+      });
 });
 
 
 describe('Collection Suite', () => {
 
-  describe('filter', () => {
+    describe('filter', () => {
 
-        it('filter based on instance', () => {
+          it('filter based on instance', () => {
 
-            const result = _.filter(users, { 'user': 'fred', 'active': false });      
-            assert.deepNestedInclude(result, users[1]);
-        });
-    });      
+              const result = _.filter(users, { 'user': 'fred', 'active': false });      
+              assert.deepNestedInclude(result, users[1]);
+          });
+      });      
 });
-
 
 describe('Lang Suite', () => {
 
@@ -77,15 +76,49 @@ describe('Lang Suite', () => {
         });
     });      
 
-
     describe('isEqual', () => {
 
-      // https://lodash.com/docs/4.17.11#isEqual
+        // https://lodash.com/docs/4.17.11#isEqual
+        it('isEqual to compare object', () => { 
+            
+            assert.isTrue(_.isEqual({}, {}));
+            assert.isTrue(_.isEqual({a:1}, {a:1}));
+            assert.isFalse(_.isEqual({a:1, b:2}, {a:1}));
+            assert.isFalse(_.isEqual({a:1}, {a:333}));
+        });
+    });
+
+    describe('isMatch', () => {
+
+      // https://lodash.com/docs/4.17.11#isMatch
       it('isEqual to compare object', () => { 
           
-          assert.isTrue(_.isEqual({}, {}));
-          assert.isTrue(_.isEqual({a:1}, {a:1}));
-          assert.isFalse(_.isEqual({a:1}, {a:333}));
+          assert.isTrue(_.isMatch({}, {}));
+          assert.isTrue(_.isMatch({a:1}, {a:1}));
+          assert.isTrue(_.isMatch({a:1, b:2}, {a:1}));
+          assert.isTrue(_.isMatch({a:1, c:{ bar:1, foo:1, z:true} }, {a:1, c:{ foo:1} }));
+          assert.isFalse(_.isMatch({a:1}, {a:333}));
       });
-  });   
+  });
+
+    describe('isXXXXX', () => {
+
+      // https://lodash.com/docs/4.17.11#isEqual
+      it('isXXXX', () => { 
+          
+          assert.isTrue(_.isNumber(1));
+          assert.isTrue(_.isInteger(1));
+          assert.isTrue(_.isArray([]));
+          assert.isTrue(_.isString(""));
+          assert.isTrue(_.isBoolean(true));
+          assert.isTrue(_.isUndefined(undefined));
+          assert.isTrue(_.isDate(new Date()));
+          assert.isTrue(_.isError(new Error()));
+          assert.isTrue(_.isFinite(1));
+          assert.isTrue(_.isFunction( () => {} ));
+          assert.isTrue(_.isObject({}) && _.isObject([]));
+          assert.isTrue(_.isPlainObject({}) && !_.isPlainObject([]));
+      });
+  });
 });
+
