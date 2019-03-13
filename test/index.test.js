a3b27867dec7fecb9e39793b01ceb588acb79f4b
Frederic Torres <fredericaltorres@gmail.com>
2019-03-13 15:29:19 -0400

    Update

--- test/index.test.js
+++ test/index.test.js
@@ -6,9 +6,9 @@ const expect = require('chai').expect;
 const assert = require('chai').assert; // https://www.chaijs.com/api/assert/
 
 var users = [
-  { 'user': 'barney',  'active': false },
-  { 'user': 'fred',    'active': false },
-  { 'user': 'pebbles', 'active': true  }
+    { 'user': 'barney',  'active': false },
+    { 'user': 'fred',    'active': false },
+    { 'user': 'pebbles', 'active': true  }
 ];
   
 describe('Array Suite', () => {
@@ -99,26 +99,83 @@ describe('Lang Suite', () => {
           assert.isTrue(_.isMatch({a:1, c:{ bar:1, foo:1, z:true} }, {a:1, c:{ foo:1} }));
           assert.isFalse(_.isMatch({a:1}, {a:333}));
       });
-  });
+    });
 
     describe('isXXXXX', () => {
 
-      // https://lodash.com/docs/4.17.11#isEqual
-      it('isXXXX', () => { 
-          
-          assert.isTrue(_.isNumber(1));
-          assert.isTrue(_.isInteger(1));
-          assert.isTrue(_.isArray([]));
-          assert.isTrue(_.isString(""));
-          assert.isTrue(_.isBoolean(true));
-          assert.isTrue(_.isUndefined(undefined));
-          assert.isTrue(_.isDate(new Date()));
-          assert.isTrue(_.isError(new Error()));
-          assert.isTrue(_.isFinite(1));
-          assert.isTrue(_.isFunction( () => {} ));
-          assert.isTrue(_.isObject({}) && _.isObject([]));
-          assert.isTrue(_.isPlainObject({}) && !_.isPlainObject([]));
-      });
-  });
+        // https://lodash.com/docs/4.17.11#isArguments
+        it('isXXXX', () => { 
+            
+            assert.isTrue(_.isNumber(1));
+            assert.isTrue(_.isInteger(1));
+            assert.isTrue(_.isArray([]));
+            assert.isTrue(_.isString(""));
+            assert.isTrue(_.isBoolean(true));
+            assert.isTrue(_.isUndefined(undefined));
+            assert.isTrue(_.isNull(null));
+            assert.isTrue(_.isDate(new Date()));
+            assert.isTrue(_.isError(new Error()));
+            assert.isTrue(_.isFinite(1));
+            assert.isTrue(_.isFunction( () => {} ));
+            assert.isTrue(_.isObject({}) && _.isObject([]));
+            assert.isTrue(_.isPlainObject({}) && !_.isPlainObject([]));
+        });
+    });
+
+    describe('Number', () => {
+
+        // https://lodash.com/docs/4.17.11#inRange
+        it('inRange', () => { 
+            
+            assert.isTrue(_.inRange(3, 3, 4));
+            assert.isTrue(_.inRange(3.5, 3, 4));
+        });
+
+        // https://lodash.com/docs/4.17.11#random
+        it('random', () => { 
+            
+            const n = _.random(0, 5);          
+            assert.isTrue(n >= 0 && n <= 5);
+
+            const nn = _.random(1.2, 5.2);
+            assert.isTrue(nn >= 1.2 && nn <= 5.2);
+        });
+    });
+
+    describe('Object', () => {
+
+        function Bar() {
+          this.c = 3;
+        }
+        Bar.prototype.d = 4;
+        // https://lodash.com/docs/4.17.11#assign
+        it('assign', () => { // <= Ignore inherited properties
+            
+            const o = _.assign({}, {a:1}, new Bar() );
+            assert.deepEqual(o, {a:1, c:3});
+        });
+
+        it('assignIn', () => { // <= Used inherited properties
+      
+            const o = _.assignIn({}, {a:1}, new Bar() );
+            assert.deepEqual(o, {a:1, c:3, d:4});
+        });
+
+        it('defaults', () => { // Combine 2 objects, used first property defined
+      
+            const o = _.defaults({ 'a': 1 }, { 'b': 2 }, { 'a': 3 });
+            assert.deepEqual(o, { a:1, b:2 });
+        });
+
+        it('defaultsDeep', () => { // Combine 2 objects, used first property defined
+      
+          const o = _.defaultsDeep({ 'a': { 'b': 2 } }, { 'a': { 'b': 1, 'c': 3 } });
+          assert.deepEqual(o, { 'a': { 'b': 2, 'c': 3 } });
+        }); 
+  });    
 });
+
+
+
+
 
